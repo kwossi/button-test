cffdce721b6a64b14d8dddd492b13f092d54bce9
kwossi <krissi.stoecker@gmail.com>
2024-10-28 15:49:44 +0100

    all

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "test-component";
+
+function App() {
+  return (
+    <>
+      <Button color="secondary">Na</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/props/buttonProps.js
@@ -0,0 +1,13 @@
+export const buttonProps = {
+  color: "rgb(255, 16, 74)",
+  children: "Dont Click",
+  onClick: function () {
+    console.log("clicked");
+  },
+  style: {
+    padding: "15px",
+    borderRadius: "10px",
+    fontSize: "18px",
+    border: "5px solid green",
+  },
+};
